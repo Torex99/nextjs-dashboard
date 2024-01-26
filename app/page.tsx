8f41f4f06b1c741457407566bc76688c61b1e904
RufusOludare <157369022+RufusOludare@users.noreply.github.com>
2024-01-26 11:02:25 +0000

    update invoice

--- app/page.tsx
+++ app/page.tsx
@@ -12,26 +12,38 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape} />
-          <p
+        <div className="flex flex-col justify-center gap-6 rounded-lg px-6 py-10 md:w-2/5 md:px-20 ">
+          <Image
+            src="/logoNorth.jpeg"
+            alt="northlink logo"
+            width={100}
+            height={100}
+            className="rounded-lg block mx-auto w-auto opacity-100"
+          />
+          <h2
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> Here you can track{' '}
+            <a href="/dashboard/invoices" className="text-blue-500">
+              invoice status
+            </a>{' '}
+            and{' '}
+            <a href="/dashboard/invoices/create" className="text-blue-500">
+              create invoices.
+            </a>{' '}
+            Brought to you by{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Northlink digital
             </a>
-            , brought to you by Vercel.
-          </p>
+          </h2>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
@@ -40,11 +52,12 @@ export default function Page() {
             alt="screenshots of the dashboard project showing desktop version"
           />
           <Image
-          src="/hero-mobile.png"
-          width={560}
-          height={620}
-          className="block md:hidden"
-          alt="Screenshots of the dashboard project showing mobile version" />
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
